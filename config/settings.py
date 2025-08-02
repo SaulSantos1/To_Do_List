5196bc0f8fa7347c96b0f8a3c7b3b5b89ed90ab5
SaulSantos1 <saulsantos111@gmail.com>
2025-08-01 22:36:19 -0300

    :wrench: chore: Adicionando funcionalidade de excluir arquivos de imagem se forem deletados

--- config/settings.py
+++ config/settings.py
@@ -20,12 +20,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_cleanup.apps.CleanupConfig",
 
-    'rest_framework',
-    'rest_framework_simplejwt',
-    'corsheaders',
-    'todolist',
-    'users',
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "corsheaders",
+    "todolist",
+    "users",
 ]
 
 MIDDLEWARE = [
